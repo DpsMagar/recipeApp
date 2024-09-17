a13b85a396749380f0d5217f4a83d3cdf7321534
DpsMagar <dipeshpaharai@gmail.com>
2024-09-17 17:45:21 +0545

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -138,7 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = ['https://recipe-app-omega-ruby.vercel.app/',]
+CORS_ALLOWED_ORIGINS = ['https://recipe-app-omega-ruby.vercel.app',]
 
 
 CORS_ALLOW_CREDENTIALS = True
